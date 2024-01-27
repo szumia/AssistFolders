abdf2da8813ce8a5664c65ec21629e3b298374af
szumia <108209195+szumia@users.noreply.github.com>
2024-01-28 00:31:02 +0800

    Delete droneRules directory

--- droneRules/creat_ttyudev.sh
+++
@@ -1,6 +0,0 @@
-sudo cp ./ttyudev.rules /etc/udev/rules.d/
-sudo udevadm control --reload-rules 
-sudo udevadm trigger
-# sudo service udev reload
-# sudo service udev restart
-echo "finish"
--- droneRules/create_mvudev.sh
+++
@@ -1,4 +0,0 @@
-sudo cp ./mvusb.rules /etc/udev/rules.d/
-sudo udevadm control --reload-rules 
-sudo udevadm trigger
-echo "finish"
